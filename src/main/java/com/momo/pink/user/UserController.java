a668b5202dff2d89799f42d3b7f3bce8f6b400ae
qiyi <bphanzhu@gmail.com>
2017-09-25 23:16:16 +0800

    修改 REST API 的 basePath 为 /api/v1.0/

--- src/main/java/com/momo/pink/user/UserController.java
+++ src/main/java/com/momo/pink/user/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-@RequestMapping("/users")
+@RequestMapping("api/v1.0/users")
 public class UserController {
 
     @Autowired
