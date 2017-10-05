6177ad1e7147218b82996f427ac1721af33bf2b1
qiyi <bphanzhu@gmail.com>
2017-10-06 01:40:28 +0800

    使用for循环加载todo数据

--- src/main/resources/public/js/pink.js
+++ src/main/resources/public/js/pink.js
@@ -2,6 +2,14 @@ Vue.use(VueMaterial)
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Pink!'
+        message: 'Hello Pink!',
+        todos: [{
+            "title": "Support transaction in pink",
+            "completeAt": "Oct 3, 2017",
+            "status": "CREATED"
+        }, {
+            "title": "Actrually not done",
+            "status": "COMPLETED"
+        }]
     }
 });
